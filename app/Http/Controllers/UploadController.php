06bb532558a8edbec1905e2f4ca7c7327413b8f1
Elvis <elvistavares@outlook.com>
2021-02-18 17:10:13 -0300

    config up

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -19,10 +19,32 @@ class UploadController extends Controller
 
     public function store(Request $request){
         //dd($request->all());
+        dd($request->nome);
+        //$dados = $request->all();
+        //Imagem::create($dados);
+        //$dados = $request->nome;
+        $dados = $request->only(['nome', 'imagem']);
+        dd($dados);
+        
+      if ($request->file('imagem')->isValid()) {
+          # code...
+         // $nome_arquivo = $request->name . "." .$request->imagem->extension();
+         //dd($request->file('imagem')->store('img'));
 
-        $dados = $request->all();
-        Imagem::create($dados);
-        return back()->with(['success'=>'OK']);
+         $nome_arqu = $request->nome . "." . $request->imagem->extension();
+         $request->file('imagem')->storeAs('img',$nome_arqu); 
+
+
+
+         
+      }
+
+      
+
+
+      
+
+   
 
     }
 }
