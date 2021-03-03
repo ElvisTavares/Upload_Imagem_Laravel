3315f3303eff3089248016963d9055459e38c9bf
Elvis <elvistavares@outlook.com>
2021-03-03 16:12:15 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Route::get('up', 'UploadController@create');
 Route::post('up', 'UploadController@store');
-Route::get('list', 'UploadController@index');
+Route::get('lists', 'UploadController@index');
